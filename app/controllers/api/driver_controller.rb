7c34985ee0c90537d4da5d977ad62149aab10574
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-02-11 10:50:26 +0300

    key

--- app/controllers/api/driver_controller.rb
+++ app/controllers/api/driver_controller.rb
@@ -11,10 +11,22 @@ class Api::DriverController < ApplicationController
       { key: ENV['RUCAPTCHA_KEY'], body: capcha['base64jpg'], method: 'base64' }
     )
     id = rucaptcha.body.split('|').last
-    sleep 7.seconds
-    resp = RestClient.get("http://rucaptcha.com/res.php?key=#{ENV['RUCAPTCHA_KEY']}&action=get&id=#{id}")
+    x = 0
+    resp = get(id)
+    while x < 10
+      x += 1
+      resp = get(id)
+      puts "#{x}: #{resp.body}"
+      resp.body[0..1] == 'OK' ? (x = 10) : sleep(1.second)
+    end
     string = "num=#{params[:num]}&date=#{params[:date]}&captchaWord=#{resp.body.split('|').last}&captchaToken=#{capcha['token']}"
     driver = JSON.parse RestClient.post('https://xn--b1afk4ade.xn--90adear.xn--p1ai/proxy/check/driver', string)
     render status: :ok, json: driver
+  end
+
+  private
+
+  def get(id)
+    RestClient.get("http://rucaptcha.com/res.php?key=#{ENV['RUCAPTCHA_KEY']}&action=get&id=#{id}")
   end
 end
