89574be0902fe5283bef46051f77ff3486dc18eb
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-03-05 23:40:03 +0300

    esia

--- app/controllers/api/femida/esia_controller.rb
+++ app/controllers/api/femida/esia_controller.rb
@@ -6,7 +6,7 @@ class Api::Femida::EsiaController < ApplicationController
   HOST = 'https://esia.gosuslugi.ru'
   PATH = 'api/public/v2'
 
-  api :GET, '/esia?phone=79991112233&passport=1234567890 /esia?email=a@a.a&inn=111222333444', 'Проверка ЕСИА'
+  api :GET, "/esia?phone=79991112233&passport=1234567890 \n/esia?email=a@a.a&inn=111222333444 \n/esia?email=a@a.a&snils=111222333444", 'Проверка ЕСИА'
   def index
     type = get("/captcha/#{PATH}/type")
     headers = { captchaSession: type['captchaSession'] }
@@ -17,11 +17,18 @@ class Api::Femida::EsiaController < ApplicationController
     search = if params[:phone]
                phone = params[:phone].last(10)
                "mbt=+7(#{phone[0..2]})#{phone.last(7)}"
-             else
+             elsif params[:email]
                "eml=#{params[:email]}"
+             elsif params[:passport]
+               "serNum=#{params[:passport]}"
+             elsif params[:inn]
+               "inn=#{params[:inn]}"
+             elsif params[:snils]
+               "snils=#{params[:snils]}"
              end
     sleep 1
-    json = get("/esia-rs/#{PATH}/recovery/find?#{search}&verifyToken=#{token['verify_token']}")
+    json = {}
+    json.merge!(get("/esia-rs/#{PATH}/recovery/find?#{search}&verifyToken=#{token['verify_token']}")) if search
     document = if params[:passport]
                  "serNum=#{params[:passport]}"
                elsif params[:inn]
@@ -29,8 +36,8 @@ class Api::Femida::EsiaController < ApplicationController
                elsif params[:snils]
                  "snils=#{params[:snils]}"
                end
-    resp = get("/esia-rs/#{PATH}/recovery/find?#{document}&requestId=#{json['requestId']}", headers2)
-    render status: :ok, json: resp
+    json.merge!(get("/esia-rs/#{PATH}/recovery/find?#{document}&requestId=#{json['requestId']}", headers2)) if document
+    render status: :ok, json: json
   end
 
   private
@@ -39,5 +46,8 @@ class Api::Femida::EsiaController < ApplicationController
     puts path
     resp = RestClient.get(HOST + path, headers)
     parse ? JSON.parse(resp) : resp
+  rescue RestClient::NotFound, RestClient::BadRequest => e
+    errors = { 400 => 'passport or snils or inn not found', 404 => 'user not found' }
+    { error: errors[e.http_code] }
   end
 end
