7ebae034978d489cf90ead02776a13f506659db2
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-03-20 13:00:00 +0300

    bonalog

--- app/controllers/api/femida/bonalog_controller.rb
+++ app/controllers/api/femida/bonalog_controller.rb
@@ -2,10 +2,34 @@
 
 class Api::Femida::BonalogController < ApplicationController
   protect_from_forgery with: :null_session
+  URL = 'https://bo.nalog.ru/nbo'
 
   api :GET, '/bonalog?query=string', 'Проверка бух/фин отчетности- (https://bo.nalog.ru/)'
   def index
-    json = JSON.parse RestClient.get("https://bo.nalog.ru/nbo/organizations/search?query=#{CGI.escape params[:query]}")
-    render status: :ok, json: json['content']
+    response = get("organizations/search?query=#{CGI.escape params[:query]}")['content'].first
+    response[:info] = get("organizations/#{response['id']}/bfo").map do |resp|
+      {
+        correctionList:       get("cb/getCbCorrectionList?organizationId=#{response['id']}&period=#{resp['period']}"),
+        organizations:        get("bfo/#{resp['id']}/organizations"),
+        details:              get("bfo/#{resp['id']}/details").map do |detail|
+          {
+            balance:          get("details/balance?id=#{detail['id']}"),
+            financial_result: get("details/financial_result?id=#{detail['id']}"),
+            capital_change:   get("details/capital_change?id=#{detail['id']}"),
+            funds_movement:   get("details/funds_movement/v2?id=#{detail['id']}")
+          }
+        end
+      }
+    end
+
+    render status: :ok, json: response
+  end
+
+  private
+
+  def get(url)
+    JSON.parse RestClient.get("#{URL}/#{url}")
+  rescue
+    {}
   end
 end
