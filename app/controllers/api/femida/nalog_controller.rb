16d483543002d9c4cbac985b3dc98b409c49b851
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-04-05 14:06:14 +0300

    nalog

--- app/controllers/api/femida/nalog_controller.rb
+++ app/controllers/api/femida/nalog_controller.rb
@@ -3,68 +3,107 @@
 class Api::Femida::NalogController < ApplicationController
   protect_from_forgery with: :null_session
 
-  api :GET, '/nalog/:inn', 'Проверка ФЛ на дисквалификацию, ограничение и ИП (https://pb.nalog.ru/search-proc.json)'
-  def show
-    @errors = []
-    resp = begin
-             load
-           rescue Errno::ECONNRESET => e
-             load
-           end
+  RETRY = 5
 
-    data = JSON.parse(resp)
-    data.delete 'captchaRequired'
-    # data = json.filter { |_, value| value['rowCount'] > 0 }
-    data.each do |key, value|
-      data[key] = value['rowCount'] > 0 ? value['data'].map { |v| transform(key, v) } : []
-    end
+  api :GET, '/nalog/ogr?id=:inn', 'Проверка на ограничение' # 668608997290
+  def ogr
+    hash = captcha_proc
+    hash[:mode] = 'search-ogr'
+    hash[:queryOgr] = params[:id]
+    d = search_proc(hash)
+    data = d['ogrfl']['data'] + d['ogrul']['data']
+    render status: :ok, json: { data: data, company: company_proc(data) }
+    # {"success":0,"limit_org":[{"name":"","inn":"","position":"","reason":"","start_date":"","end_date":"","org_name":"","org_inn":""}],"error":""}
+  end
 
-    render status: :ok, json: data.merge(success: data.size, error: @errors)
+  api :GET, '/nalog/uchr?id=:inn', 'Проверка на учредителей и гендиректоров' # 502419236001
+  def uchr
+    hash = captcha_proc
+    hash[:mode] = 'search-upr-uchr'
+    hash[:queryUpr] = params[:id]
+    hash[:uprType0] = 1
+    hash[:uprType1] = 1
+    d = search_proc(hash)
+    data = d['uchr']['data'] + d['upr']['data']
+    resp = data.map do |z|
+      hash = { pbCaptchaToken: hash[:pbCaptchaToken], token: z['token'], mode: 'search-ul', queryUpr: z['inn'] }
+      x = search_proc(hash)
+      next unless x
+
+      x['ul']['data']
+    end.compact.flatten
+    render status: :ok, json: { data: data, resp: resp, company: company_proc(resp, hash[:pbCaptchaToken]) }
+    # {"success":0,"director":[{"inn":"","name":"","count":0}],"owner":[{"inn":"","name":"","count":0}],"error":""}
+  end
+
+  api :GET, '/nalog/ip?id=:inn', 'Проверка на ИП' # 772830410106
+  def ip
+
+    # {"success":0,"ip":[{"ogrn":"","okved":"","okved_name":"","name":""}],"error":""}
+  end
+
+  api :GET, '/nalog/rdl?id=:inn', 'Проверка на дисквалификацию' # 5205037677
+  def rdl
+
+    # {"success":0,"dis":[{"number":"","name":"","date_of_birth":"","place_of_birth":"","name_org":"","position":"","article":"","creator":"","court":"","period":"","start_date":"","end_date":""}],"error":""}
   end
 
   private
 
-  def load
-    RestClient::Request.execute(url: 'https://pb.nalog.ru/search-proc.json', payload: "mode=search-all&queryAll=#{params[:id]}&queryUl=&okvedUl=&statusUl=&regionUl=&isMspUl=&mspUl1=1&mspUl2=2&mspUl3=3&queryIp=&okvedIp=&statusIp=&regionIp=&isMspIp=&mspIp1=1&mspIp2=2&mspIp3=3&uprType1=1&uprType0=1&ogrFl=1&ogrUl=1&npTypeDoc=1", method: :post, verify_ssl: false)
+  def company_proc(array, pbCaptchaToken = nil)
+    array.map do |z|
+      hash = { token: z['token'], method: 'get-request' }
+      hash[:pbCaptchaToken] = pbCaptchaToken if pbCaptchaToken
+      resp = load_retry('https://pb.nalog.ru/company-proc.json', prepare_hash(hash))
+      next if resp.nil?
+
+      x = JSON.parse(resp)
+      hash = { token: x['token'], id: x['id'], method: 'get-response' }
+
+      JSON.parse(load_retry('https://pb.nalog.ru/company-proc.json', prepare_hash(hash)))
+    end.compact
   end
 
-  def transform(key, data)
-    case key
-    when 'uchr'
-      {
-        type:   data['type'],
-        ul_cnt: data['ul_cnt'],
-        name:   data['name']
-      }
-    when 'upr'
-      data
-    when 'ul'
-      {
-        region:     data['regionname'],
-        okved:      data['okved2'],
-        okved_name: data['okved2name'],
-        full_name:  data['namep'],
-        name:       data['namec']
-      }
-    when 'ogrfl'
-      data
-    when 'docul'
-      data
-    when 'rdl'
-      data
-    when 'addr'
-      data
-    when 'docip'
-      data
-    when 'ogrul'
-      data
-    when 'ip'
-      {
-        ogrn:       data['ogrn'],
-        okved:      data['okved2'],
-        okved_name: data['okved2name'],
-        name:       data['namec']
-      }
+  def search_proc(hash)
+    hash[:ogrFl] = 1
+    hash[:ogrUl] = 1
+    JSON.parse(load_retry('https://pb.nalog.ru/search-proc.json', prepare_hash(hash)))
+  end
+
+  def prepare_hash(hash)
+    hash.map { |key, value| "#{key}=#{value}" }.join("&")
+  end
+
+  def captcha_proc
+    z = RestClient.get('https://pb.nalog.ru/static/captcha.bin?version=2').body
+    capcha = RestClient.get("https://pb.nalog.ru/static/captcha.bin?version=2&a=#{z}")
+    @resp = post_rucaptcha(Base64.encode64(capcha.body), phrase: 0, regsense: 0, numeric: 1, language: 0)
+    return if @resp == ApplicationController::ERROR
+
+    proc = load_retry('https://pb.nalog.ru/captcha-proc.json', "captcha=#{@resp.split('|').last}&captchaToken=#{z}")
+    proc = proc.delete("\"") if proc
+
+    { pbCaptchaToken: proc, token: z }
+  end
+
+  def load_retry(url, payload)
+    puts "--------------------------------------------------------- >>"
+    puts "#{url} => #{payload}"
+
+    x = 0
+    resp = nil
+    while x < RETRY
+      x += 1
+      resp = load(url, payload)
+      resp.nil? ? sleep(1.second) : (x = RETRY)
     end
+    puts "--------------------------------------------------------- <<"
+    puts resp
+    resp
+  end
+
+  def load(url, payload)
+    RestClient::Request.execute(url: url, payload: payload, method: :post, verify_ssl: false).body
+    rescue Errno::ECONNRESET, RestClient::BadRequest
   end
 end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'http://4d74c450486d4ac8af601e6ee039d1de@femida-search.online:9000/2'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
