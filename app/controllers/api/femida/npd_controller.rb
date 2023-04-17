1bceb0ecd9cd88e72a22e8adda3b96a8321c52e8
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-04-17 16:46:19 +0300

    error_handling

--- app/controllers/api/femida/npd_controller.rb
+++ app/controllers/api/femida/npd_controller.rb
@@ -1,17 +1,33 @@
 # frozen_string_literal: true
 
 class Api::Femida::NpdController < ApplicationController
+  RETRY = 5
+
   protect_from_forgery with: :null_session
 
   api :GET, '/npd/:inn', 'Проверка самозанятого - json: {"status": boolean, "message": string} (https://npd.nalog.ru/check-status/)'
   def show
-    with_error_handling do
-      body = RestClient.post(
-        'https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status',
-        { inn: params[:id].to_s, requestDate: params[:date] || Date.current.to_s }.to_json,
-        content_type: 'application/json'
-      )
-      JSON.parse(body)
+    x = 0
+    while x < RETRY
+      @error = nil
+      x += 1
+      resp = post_request
+      puts x
+      resp.try(:code) == 200 ? (x = RETRY) : sleep(1.second)
     end
+
+    render status: :ok, json: @error.present? ? { status: false, error: @error } : JSON.parse(resp.body)
+  end
+
+  private
+
+  def post_request
+    RestClient.post(
+      'https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status',
+      { inn: params[:id].to_s, requestDate: params[:date] || Date.current.to_s }.to_json,
+      content_type: 'application/json'
+    )
+  rescue RestClient::UnprocessableEntity => e
+    @error = e.message
   end
 end
