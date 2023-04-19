b5e01f4135cce8f83218f59e899375df51a6936f
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-04-19 10:39:58 +0300

    check_inn

--- app/controllers/api/femida/npd_controller.rb
+++ app/controllers/api/femida/npd_controller.rb
@@ -1,32 +1,31 @@
 # frozen_string_literal: true
 
 class Api::Femida::NpdController < ApplicationController
-  RETRY = 5
-
   protect_from_forgery with: :null_session
 
   api :GET, '/npd/:inn', 'Проверка самозанятого - json: {"status": boolean, "message": string} (https://npd.nalog.ru/check-status/)'
   def show
-    x = 0
-    while x < RETRY
-      @error = nil
-      x += 1
-      resp = post_request
-      puts x
-      resp.try(:code) == 200 ? (x = RETRY) : sleep(1.second)
-    end
+    with_error_handling do
+      inn = check_inn(params[:id].to_s)
 
-    render status: :ok, json: @error.present? ? { status: false, error: @error } : JSON.parse(resp.body)
+      x = 0
+      while x < RETRY
+        @error = nil
+        x += 1
+        resp = post_request(inn)
+        puts x
+        resp.try(:code) == 200 ? (x = RETRY) : sleep(1)
+      end
+
+      JSON.parse(resp.body)
+    end
   end
 
   private
 
-  def post_request
-    RestClient.post(
-      'https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status',
-      { inn: params[:id].to_s, requestDate: params[:date] || Date.current.to_s }.to_json,
-      content_type: 'application/json'
-    )
+  def post_request(inn)
+    z = { inn: inn, requestDate: params[:date] || Date.current.to_s }.to_json
+    RestClient.post('https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status', z, content_type: 'application/json')
   rescue RestClient::UnprocessableEntity => e
     @error = e.message
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,9 +30,29 @@ class ApplicationController < ActionController::Base
     error = nil
     body = begin
              yield
-           rescue Errno::ECONNRESET, RestClient::NotFound, RestClient::BadRequest, StandardError, LoadError, Exception => e
+           rescue Exception => e
              error = e.message
            end
     render status: :ok, json: error.present? ? { status: false, error: error } : body
+  end
+
+  def check_inn(inn)
+    array = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    if inn.scan(/\D/).present?
+      raise 'inn must contains only digits'
+    elsif inn.size == 10
+      array.shift
+      check(inn, array) == inn[-1].to_i ? inn : raise('inn not valid')
+    elsif inn.size == 12
+      check(inn, array) == inn[-2].to_i && check(inn, [3] + array) == inn[-1].to_i ? inn : raise('inn not valid')
+    else
+      raise 'inn size must be 10 or 12 digits'
+    end
+  end
+
+  def check(inn, array)
+    sum = 0
+    inn[0..array.size-1].each_char.with_index { |x, i| sum += array[i] * x.to_i }
+    sum % 11 % 10
   end
 end
